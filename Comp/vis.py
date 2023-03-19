ac24e2a75a0998a843f08d28d867fa47e096affe
XingjianL <lixingjian2002@gmail.com>
2023-03-19 17:42:50 -0400

    upsample the dataset

--- Comp/vis.py
+++ Comp/vis.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+from typing import List
 def getDataset(dir:str):
     files_all = os.listdir(dir)
 
@@ -35,7 +36,7 @@ def getDataset(dir:str):
         #X.iloc[:,[3,4,5,-1]].plot(x = "time", ax=axes[1])
         #y.plot(x = "time", ax=axes[2])
         #plt.show()
-    #print(f"Dataset obtained: \n{X.describe()}\n{y.describe()}")
+    print(f"Dataset obtained: \n{X.describe()}\n{y.describe()}")
     return organizedX, organizedY
 
 def splitTrainVal(train_set_x, train_set_y, val_train_split = 1/5):
@@ -54,13 +55,45 @@ def splitTrainVal(train_set_x, train_set_y, val_train_split = 1/5):
     print(train_idxs)
     return train_idxs, val_idxs
 
+def CombineAndUpsample(X , y : List[pd.DataFrame]):
+    print(len(X))
+    print(len(y))
+    all_upsampled = []
+    for i in range(len(y)):
+        X_i = X[i]
+        y_i = y[i]
+        print(X_i)
+        print(y_i)
+        combined = pd.merge(X_i, y_i, how='outer', on='time', sort='True')
+        upsampled = combined.interpolate(method='linear')
+        
+        upsampled['label'] = upsampled['label'].fillna(0)
+        print(upsampled['label'].isna().sum())
+        upsampled['label'] = upsampled['label'].astype(int)
+        all_upsampled.append(upsampled)
+        print(upsampled)
+    return all_upsampled
 
 if __name__ == "__main__":
     train_dir = '/home/lixin/Classes/Spr23/542/Projects-ECE542/Comp/data/TrainingData/'
-    train_set_x, train_set_y = getDataset(train_dir)
-    print(len(train_set_x))
-    total_samples = sum([len(x) for x in train_set_x])
+    X, y = getDataset(train_dir) # list of X dataframe each with [imu1-6, time], list of y dataframe each with [label, time]
+    print(len(X))
+    total_samples = sum([len(x) for x in X])
     print(total_samples)
-    train_idxs, val_idxs = splitTrainVal(train_set_x, train_set_y)
+    train_idxs, val_idxs = splitTrainVal(X, y)
+    val_set_x = [X[i] for i in val_idxs]
+    val_set_y = [y[i] for i in val_idxs]
+    train_set_x = [X[i] for i in train_idxs]
+    train_set_y = [y[i] for i in train_idxs]
+    training_set = CombineAndUpsample(train_set_x, train_set_y)
+
+    fig, axes = plt.subplots(nrows=3, ncols=2)
+    X[0].iloc[:,[0,1,2,-1]].plot(x = 'time', ax=axes[0,1])
+    X[0].iloc[:,[3,4,5,-1]].plot(x = 'time', ax=axes[1,1])
+    y[0].plot(x = 'time', ax=axes[2,1])
+    training_set[0].iloc[:,[0,1,2,-2]].plot(x = 'time', ax=axes[0,0])
+    training_set[0].iloc[:,[3,4,5,-2]].plot(x = 'time', ax=axes[1,0])
+    training_set[0].iloc[:,[-1,-2]].plot(x = 'time', ax=axes[2,0])
+    plt.show()
     #test_dir = './data/TestData/'
     #test_set = getDataset(test_dir)
