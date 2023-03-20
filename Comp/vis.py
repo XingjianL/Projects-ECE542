3ca8f4c786c4e5ec3d99c9aeba1ae3732bc1eab3
XingjianL <lixingjian2002@gmail.com>
2023-03-19 20:31:43 -0400

    one hot encoding

--- Comp/vis.py
+++ Comp/vis.py
@@ -36,7 +36,7 @@ def getDataset(dir:str):
         #X.iloc[:,[3,4,5,-1]].plot(x = "time", ax=axes[1])
         #y.plot(x = "time", ax=axes[2])
         #plt.show()
-    print(f"Dataset obtained: \n{X.describe()}\n{y.describe()}")
+    #print(f"Dataset obtained: \n{X.describe()}\n{y.describe()}")
     return organizedX, organizedY
 
 def splitTrainVal(train_set_x, train_set_y, val_train_split = 1/5):
@@ -48,52 +48,87 @@ def splitTrainVal(train_set_x, train_set_y, val_train_split = 1/5):
         new_sample = np.random.choice([x for x in range(len(train_set_x)) if x not in val_idxs])
         val_idxs.append(new_sample)
         val_samples = sum([len(train_set_x[idx]) for idx in val_idxs])
-    print(val_samples)
-    print(val_samples/total_samples)
-    print(val_idxs)
+    #print(val_samples)
+    #print(val_samples/total_samples)
+    #print(val_idxs)
     train_idxs = [x for x in range(len(train_set_x)) if x not in val_idxs]
-    print(train_idxs)
+    #print(train_idxs)
     return train_idxs, val_idxs
 
-def CombineAndUpsample(X , y : List[pd.DataFrame]):
-    print(len(X))
-    print(len(y))
+def combineAndUpsample(X : List[pd.DataFrame], y : List[pd.DataFrame], consistent_times = True):
+    #print(len(X))
+    #print(len(y))
     all_upsampled = []
     for i in range(len(y)):
         X_i = X[i]
         y_i = y[i]
-        print(X_i)
-        print(y_i)
+        #print(X_i)
+        #print(y_i)
         combined = pd.merge(X_i, y_i, how='outer', on='time', sort='True')
         upsampled = combined.interpolate(method='linear')
         
         upsampled['label'] = upsampled['label'].fillna(0)
-        print(upsampled['label'].isna().sum())
         upsampled['label'] = upsampled['label'].astype(int)
+
+        if consistent_times:
+            rows_to_remove = upsampled[((((upsampled['time'] / 0.025) % 1) > 0.001) & ((upsampled['time'] % 0.025) < 0.024))].index
+            #print(rows_to_remove)
+            #print(0.075 % 0.025)
+            upsampled.drop(rows_to_remove, inplace=True)
+        
         all_upsampled.append(upsampled)
-        print(upsampled)
+
+        #print(upsampled)
     return all_upsampled
 
-if __name__ == "__main__":
-    train_dir = '/home/lixin/Classes/Spr23/542/Projects-ECE542/Comp/data/TrainingData/'
-    X, y = getDataset(train_dir) # list of X dataframe each with [imu1-6, time], list of y dataframe each with [label, time]
-    print(len(X))
-    total_samples = sum([len(x) for x in X])
-    print(total_samples)
+def oneHotEncode(dataset_list):
+    for dataset in dataset_list:
+        dataset["label_0"] = (dataset["label"] == 0).astype(int)
+        dataset["label_1"] = (dataset["label"] == 1).astype(int)
+        dataset["label_2"] = (dataset["label"] == 2).astype(int)
+        dataset["label_3"] = (dataset["label"] == 3).astype(int)
+
+    return dataset_list
+
+
+def prepareData(filepath : str, plot = False, consistent_times = True, one_hot_encoding = True):
+    # list of X dataframe each with [imu1-6, time], list of y dataframe each with [label, time]
+    X, y = getDataset(filepath) 
+    
     train_idxs, val_idxs = splitTrainVal(X, y)
     val_set_x = [X[i] for i in val_idxs]
     val_set_y = [y[i] for i in val_idxs]
     train_set_x = [X[i] for i in train_idxs]
     train_set_y = [y[i] for i in train_idxs]
-    training_set = CombineAndUpsample(train_set_x, train_set_y)
 
-    fig, axes = plt.subplots(nrows=3, ncols=2)
-    X[0].iloc[:,[0,1,2,-1]].plot(x = 'time', ax=axes[0,1])
-    X[0].iloc[:,[3,4,5,-1]].plot(x = 'time', ax=axes[1,1])
-    y[0].plot(x = 'time', ax=axes[2,1])
-    training_set[0].iloc[:,[0,1,2,-2]].plot(x = 'time', ax=axes[0,0])
-    training_set[0].iloc[:,[3,4,5,-2]].plot(x = 'time', ax=axes[1,0])
-    training_set[0].iloc[:,[-1,-2]].plot(x = 'time', ax=axes[2,0])
-    plt.show()
+    # combine X and y and upsample and interpolate to X_time
+    training_set = combineAndUpsample(train_set_x, train_set_y, consistent_times=consistent_times)
+    validation_set = combineAndUpsample(val_set_x, val_set_y, consistent_times=consistent_times)
+
+    if plot:
+        fig, axes = plt.subplots(nrows=3, ncols=2)
+        axes[0,0].title.set_text("Interpolated Data \n(linear, should be same as original plot)")
+        axes[0,1].title.set_text("Original Data")
+        X[0].iloc[:,[0,1,2,-1]].plot(x = 'time', ax=axes[0,1])
+        X[0].iloc[:,[3,4,5,-1]].plot(x = 'time', ax=axes[1,1])
+        y[0].plot(x = 'time', ax=axes[2,1])
+        training_set[0].iloc[:,[0,1,2,-2]].plot(x = 'time', ax=axes[0,0])
+        training_set[0].iloc[:,[3,4,5,-2]].plot(x = 'time', ax=axes[1,0])
+        training_set[0].iloc[:,[-1,-2]].plot(x = 'time', ax=axes[2,0])
+        plt.show()
+
+    if one_hot_encoding:
+        training_set = oneHotEncode(training_set)
+        validation_set = oneHotEncode(validation_set)
+    print(f"Dataset obtained: training {len(training_set)}, val {len(validation_set)}. \
+          \n datapoints: train {sum([len(x) for x in training_set])}, val {sum([len(x) for x in validation_set])}.")
+    print(f"Example (training_set[0]):\n{training_set[0].head(3)}\n")
+    return training_set, validation_set
+
+if __name__ == "__main__":
+    train_dir = '/home/lixin/Classes/Spr23/542/Projects-ECE542/Comp/data/TrainingData/'
+    training_set, validation_set = prepareData(train_dir, plot=True)
+
+    
     #test_dir = './data/TestData/'
     #test_set = getDataset(test_dir)
